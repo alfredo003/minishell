9af3838bf011fe2abfdc41596356aa1051661d8e
11StarLord <diogowalter114@gmail.com>
2024-11-15 22:59:34 +0100

    Adding execute_internal_command to minishell.c

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -20,19 +20,45 @@ void	execute_external_command(char **args)
 		wait(&status); // Espera o processo filho terminar
 }
 
+int	execute_internal_command(char **args)
+{
+	if (ft_strcmp(args[0], "cd") == 0)
+	{
+		if (args[1] == NULL)
+			ft_printf("mini_shell: waiting for \"cd dir_name\"\n");
+		else
+		{
+			if (chdir(args[1]) != 0)
+				perror("mini_shell");
+		}
+		return (1);
+	}
+	if (ft_strcmp(args[0], "exit") == 0)
+		exit(0);
+	return (0);
+}
+
 void	execute_command(char *input)
 {
 	char	**args;
+	int	i;
 
 	args = ft_split(input, ' ');
-	if (args[0] == NULL)
+	i = 0;
+	if (args[i] == NULL)
 	{
 		free(args);
 		return ;
 	}
-	execute_external_command(args);
+	if (!execute_internal_command(args))
+		execute_external_command(args);
+	while (args[i] != NULL)
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
 }
-
 
 int	main(void)
 {

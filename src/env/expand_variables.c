4c4489337381f7132e107787c04aebd676682224
Alfredo Manuel <alfredochivela3@gmail.com>
2025-01-15 11:18:23 +0100

    updated

--- src/env/expand_variables.c
+++ src/env/expand_variables.c
@@ -68,44 +68,55 @@ void	handle_variable_expansion(t_shell *shell
 	ft_free(env_value, 1);
 }
 
-void	expand_variables_loop(t_shell *shell, char *input, char *expanded,
-	int in_heredoc)
+void expand_variables_loop(t_shell *shell, char *input, char *expanded, int in_heredoc)
 {
-	int		n;
-	int		j;
+    int n = 0;
+    int j = 0;
 
-	n = 0;
-	j = 0;
-	shell->values.val1 = 0;
-	if (!expanded)
-		return ;
-	while (input[n])
-	{
-		if (input[n] == '$' && input[n + 1]
-			&& (shell->values.val1 != 39 || in_heredoc))
-		{
-			shell->values.str1 = input;
-			handle_variable_expansion(shell, expanded, &n, &j);
-		}
-		else
-		{
-			if (ft_can_be_add(input, n, shell))
-				expanded[j++] = input[n];
-			handle_quotes(shell, input, &n);
-			n++;
-		}
-	}
-	expanded[j] = '\0';
+    shell->values.val1 = 0;
+
+    if (!expanded) // Segurança extra, embora não devesse acontecer
+        return;
+
+    while (input[n])
+    {
+        if (input[n] == '$' && input[n + 1] && (shell->values.val1 != 39 || in_heredoc))
+        {
+            shell->values.str1 = input; // Configura a variável de ambiente
+            handle_variable_expansion(shell, expanded, &n, &j);
+        }
+        else
+        {
+            if (ft_can_be_add(input, n, shell))
+                expanded[j++] = input[n]; // Copia o caractere atual
+            handle_quotes(shell, input, &n); // Lida com aspas
+            n++;
+        }
+    }
+    expanded[j] = '\0'; // Finaliza a string com '\0'
 }
 
-char	*expand_variables(t_shell *shell, char *input, int in_heredoc)
+char *expand_variables(t_shell *shell, char *input, int in_heredoc)
 {
-	char	*expanded;
-	int		len_aloc;
+    char *expanded;
+    int len_aloc;
 
-	len_aloc = ft_get_len_aloc(shell, input, in_heredoc);
-	expanded = (char *)malloc(len_aloc);
-	expand_variables_loop(shell, input, expanded, in_heredoc);
-	ft_free(input, 1);
-	return (expanded);
+    // Calcula o tamanho necessário para a string expandida
+    len_aloc = ft_get_len_aloc(shell, input, in_heredoc);
+
+    // Aloca espaço para a string expandida (+1 para o '\0')
+    expanded = (char *)malloc(len_aloc + 1);
+    if (!expanded)
+    {
+        ft_free(input, 1); // Libera o input caso a alocação falhe
+        return (NULL);
+    }
+
+    // Expande as variáveis e constrói a string
+    expand_variables_loop(shell, input, expanded, in_heredoc);
+
+    // Libera a entrada original
+    ft_free(input, 1);
+
+    return (expanded);
 }
